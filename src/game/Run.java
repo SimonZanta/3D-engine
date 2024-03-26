e73be532dc3e5dcd5e43349d18aedc407e59dd22
Šimon Žanta <simon.zanta@protonmail.com>
2024-03-27 00:17:57 +0100

    refactor(game:run): add comments and make it more visually pleasing

--- src/game/Run.java
+++ src/game/Run.java
@@ -3,6 +3,7 @@ package game;
 import engine.*;
 import engine.item.GameItem;
 import engine.item.Mesh;
+import game.objects.Cube;
 import org.lwjgl.opengl.GL;
 import render.GameLoop;
 import render.Renderer;
@@ -18,8 +19,13 @@ import static org.lwjgl.system.MemoryUtil.memFree;
 
 public class Run {
 
+    public static void main(String[] args) throws Exception {
+        new Run().init();
+    }
+    
     public void init() throws Exception {
 
+//      ----------- WINDOW INIT ------------------------------------------------------
         //init glfw for window creation
         glfwInit();
 
@@ -28,155 +34,49 @@ public class Run {
         //add interactivity to glfw
         glfwMakeContextCurrent(window.getWindow());
 
-        //initialize OpenGL, without this, nothing will happen
+        //initialize OpenGL
+        //don't write anything except window handling before this
         GL.createCapabilities();
 
+//      ----------- GL SETUP --------------------------------------------------------
         glViewport(0,0,800,600);
 
         //enable z-buffer
         glEnable(GL_DEPTH_TEST);
 
-        //load shaders from file
+//      ----------- SHADER SETUP ----------------------------------------------------
+
+        ShaderProgram shaderProgram = new ShaderProgram();
+        Renderer renderer = new Renderer(shaderProgram);
+
+
+        //load fragment and vertex shaders from file
         String shaderVertSrc = Utils.readFile("resources/shaders/shader.vert");
         String shaderFragSrc = Utils.readFile("resources/shaders/shader.frag");
 
-        ShaderProgram shaderProgram = new ShaderProgram();
         shaderProgram.createVertShader(shaderVertSrc);
         shaderProgram.createFragShader(shaderFragSrc);
         shaderProgram.link();
 
-        Renderer renderer = new Renderer(shaderProgram);
-
+        //create uniforms
         shaderProgram.createUniform("projectionMatrix");
         shaderProgram.createUniform("worldMatrix");
         shaderProgram.createUniform("textureSampler");
 
-        float[] colours = new float[]{
-                0.5f, 0.0f, 0.0f,
-                0.0f, 0.5f, 0.0f,
-                0.0f, 0.0f, 0.5f,
-                0.0f, 0.5f, 0.5f,
-                0.5f, 0.0f, 0.0f,
-                0.0f, 0.5f, 0.0f,
-                0.0f, 0.0f, 0.5f,
-                0.0f, 0.5f, 0.5f,
-        };
-
-        // it seems uneffective, but this way of duplicating vertecies is really the most effective
-        float[] positions = new float[] {
-                // V0
-                -0.5f, 0.5f, 0.5f,
-                // V1
-                -0.5f, -0.5f, 0.5f,
-                // V2
-                0.5f, -0.5f, 0.5f,
-                // V3
-                0.5f, 0.5f, 0.5f,
-                // V4
-                -0.5f, 0.5f, -0.5f,
-                // V5
-                0.5f, 0.5f, -0.5f,
-                // V6
-                -0.5f, -0.5f, -0.5f,
-                // V7
-                0.5f, -0.5f, -0.5f,
-
-                // For text coords in top face
-                // V8: V4 repeated
-                -0.5f, 0.5f, -0.5f,
-                // V9: V5 repeated
-                0.5f, 0.5f, -0.5f,
-                // V10: V0 repeated
-                -0.5f, 0.5f, 0.5f,
-                // V11: V3 repeated
-                0.5f, 0.5f, 0.5f,
-
-                // For text coords in right face
-                // V12: V3 repeated
-                0.5f, 0.5f, 0.5f,
-                // V13: V2 repeated
-                0.5f, -0.5f, 0.5f,
-
-                // For text coords in left face
-                // V14: V0 repeated
-                -0.5f, 0.5f, 0.5f,
-                // V15: V1 repeated
-                -0.5f, -0.5f, 0.5f,
-
-                // For text coords in bottom face
-                // V16: V6 repeated
-                -0.5f, -0.5f, -0.5f,
-                // V17: V7 repeated
-                0.5f, -0.5f, -0.5f,
-                // V18: V1 repeated
-                -0.5f, -0.5f, 0.5f,
-                // V19: V2 repeated
-                0.5f, -0.5f, 0.5f,
-        };
-        float[] texCoords = new float[]{
-                0.0f, 0.0f,
-                0.0f, 0.5f,
-                0.5f, 0.5f,
-                0.5f, 0.0f,
-
-                0.0f, 0.0f,
-                0.5f, 0.0f,
-                0.0f, 0.5f,
-                0.5f, 0.5f,
-
-                // For text coords in top face
-                0.0f, 0.5f,
-                0.5f, 0.5f,
-                0.0f, 1.0f,
-                0.5f, 1.0f,
-
-                // For text coords in right face
-                0.0f, 0.0f,
-                0.0f, 0.5f,
-
-                // For text coords in left face
-                0.5f, 0.0f,
-                0.5f, 0.5f,
-
-                // For text coords in bottom face
-                0.5f, 0.0f,
-                1.0f, 0.0f,
-                0.5f, 0.5f,
-                1.0f, 0.5f,
-        };
-
-        int[] indices = new int[]{
-                // Front face
-                0, 1, 3, 3, 1, 2,
-                // Top Face
-                8, 10, 11, 9, 8, 11,
-                // Right face
-                12, 13, 7, 5, 12, 7,
-                // Left face
-                14, 15, 6, 4, 14, 6,
-                // Bottom face
-                16, 18, 19, 17, 16, 19,
-                // Back face
-                4, 6, 7, 5, 4, 7,};
-
-
-        List<Mesh> meshList = new ArrayList<>();
-//        mesh = new engine.item.Mesh(positions, indices, colours);
-        Mesh mesh = new Mesh(positions, indices, texCoords, new Texture("/model/cube/cube.png"));
-        meshList.add(mesh);
+//      ----------- OBJECT INIT -----------------------------------------------------
+        //TODO create scene, rather than gameItems themselves
 
         List<GameItem> gameItems = new ArrayList<>();
-        gameItems.add(new GameItem(mesh));
+        GameItem cube = new Cube("/model/cube/cube.png").createGameItem();
 
+        gameItems.add(cube);
+
+//      ----------- GAME LOOP -------------------------------------------------------
         GameLoop gameLoop = new GameLoop(renderer, window);
         gameLoop.play(gameItems);
 
         //destroy window
         glfwDestroyWindow(window.getWindow());
         glfwTerminate();
-    }
-
-    public static void main(String[] args) throws Exception {
-        new Run().init();
     }
 }
