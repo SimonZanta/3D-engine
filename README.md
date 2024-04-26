90a33eded30887f620fe98ca92978c10f8875dae
Šimon Žanta <simon.zanta@protonmail.com>
2024-04-26 10:02:56 +0200

    feat(root): add console and normal README

---
+++ README.md
@@ -0,0 +1,21 @@
+## PRGF2 Semestral Project ##
+
+--------------------------------------------------
+**CONTROLS:**
+
+| ACTION               | BUTTON     |
+|----------------------|------------|
+| Movement:            | W, A, S, D |
+| Camera Look:         | Mouse      |
+| Change Cube Texture: | P          |
+| Exit Application:    | Escape     |
+--------------------------------------------------
+
+**ANIMATION:**
+
+| ACTION            | BUTTON |
+|-------------------|--------|
+| Stop:             | Space  |
+| Change Direction: | J      |
+
+--------------------------------------------------
--- src/render/WelcomeText.java
+++ src/render/WelcomeText.java
@@ -5,9 +5,15 @@ public class WelcomeText {
         System.out.println("Welcome to my showcase of normal textures");
         System.out.println("-----------------------------------------");
         System.out.println("CONTROLS:");
+        System.out.println("=========");
         System.out.println("movement: W,A,S,D");
         System.out.println("looking: mouse");
         System.out.println("change cube texture: P");
         System.out.println("exit application: escape");
+        System.out.println("-----------------------------------------");
+        System.out.println("ANIMATION:");
+        System.out.println("==========");
+        System.out.println("stop: space");
+        System.out.println("change direction: j");
     }
 }
